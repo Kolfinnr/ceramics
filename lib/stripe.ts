b80e8902a0739ddbde9ba37429a55b13c10273f9
Kolfinnr <dmytro1998ua@gmail.com>
2026-01-01 09:20:56 +0100

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,4 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-01-27.acacia", // ok if your Stripe account supports it; otherwise remove this line
-});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
