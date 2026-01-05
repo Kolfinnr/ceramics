4e513f627066e0151c5de8f143995c5cd715a44a
Kolfinnr <dmytro1998ua@gmail.com>
2026-01-05 18:05:53 +0100

    Update redis.ts

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,8 @@
 import { Redis } from "@upstash/redis";
+await redis.eval(script, [key1, key2], [arg1, arg2])
 
 export const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL!,
   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 });
+
